531bf93883e0ce40584ed831679edb1ce7273250
BBanshy <belcastromathias15@gmail.com>
2022-08-26 13:05:46 +0200

    .gitignore repair

--- index.js
+++ index.js
@@ -1,31 +1,21 @@
 const express = require("express");
-
 const expressGraphQL = require("express-graphql").graphqlHTTP;
-const cors = require('cors')
-
+const cors = require("cors");
+const schema = require("./schema");
 
 const app = express();
 
-
 app.use(
-
   "/graphql",
-
   expressGraphQL({
-
     graphiql: true,
-
+    schema,
   }),
   cors()
-
 );
-
 
 const PORT = 5000;
 
-
 app.listen(PORT, () => {
-
   console.log(`Server listening on http://localhost:${PORT}`);
-
 });
--- schema.js
+++ schema.js
@@ -1,28 +1,36 @@
 const graphql = require("graphql");
+const _ = require("lodash");
+const { GraphQLString, GraphQLObjectType, GraphQLInt, GraphQLSchema } = graphql;
 
+const users = [
+  { id: "12", firstName: "Bill", age: "35" },
 
-const { GraphQLString, GraphQLObjectType, GraphQLInt } = graphql;
-
+  { id: "20", firstName: "Seth", age: "25" },
+];
 
 const UserType = new GraphQLObjectType({
   name: "User",
+
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
   },
-
 });
 
-// Like the data's entry point
 const RootQuery = new GraphQLObjectType({
-    name: "RootQueryType",
-    fields: {
-        user: {
-            type: UserType,
-            args: {id: { type: GraphQLString } },
-            resolve(parentValue, args) {
-            },
-        }
-    }
-})
+  name: "RootQueryType",
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return _.find(users, { id: args.id });
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
